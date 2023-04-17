f662be75df7beddf6c9e7b4395cc26195714ebc8
BilalWazir <101708971+BilalWazir@users.noreply.github.com>
2023-04-17 08:16:52 +0000

    changes

--- script.js
+++ script.js
@@ -36,147 +36,33 @@ document.querySelectorAll('a[href^="#"]').forEach(anchor => {
   });
   
 
-  /* Mouse Cursor Effect*/
-  
-console.clear();
-
-const { gsap, CircleType } = window;
-
-const cursorOuter = document.querySelector(".cursor--large");
-const cursorInner = document.querySelector(".cursor--small");
-const cursorTextContainerEl = document.querySelector(".cursor--text");
-const cursorTextEl = cursorTextContainerEl.querySelector(".text");
-
-const hoverItems = document.querySelectorAll(".cursor-hover-item");
-const hoverEffectDuration = 0.3;
-let isHovered = false;
-let initialCursorHeight;
-
-const cursorRotationDuration = 8;
-
-let circleType = new CircleType(cursorTextEl);
-circleType.radius(50);
-
-setTimeout(() => {
-  initialCursorHeight = circleType.container.style.getPropertyValue("height");
-  console.log(initialCursorHeight);
-}, 50);
-
-hoverItems.forEach((item) => {
-  item.addEventListener("pointerenter", handlePointerEnter);
-  item.addEventListener("pointerleave", handlePointerLeave);
+/* Mouse Cursor Effect*/
+// create instance of kinet with custom settings
+var kinet = new Kinet({
+  acceleration: 0.06,
+  friction: 0.20,
+  names: ["x", "y"],
 });
 
-let mouse = {
-  x: -100,
-  y: -100
-};
+// select circle element
+var circle = document.getElementById('circle');
 
-document.body.addEventListener("pointermove", updateCursorPosition);
+// set handler on kinet tick event
+kinet.on('tick', function(instances) {
+  circle.style.transform = `translate3d(${ (instances.x.current) }px, ${ (instances.y.current) }px, 0) rotateX(${ (instances.x.velocity/2) }deg) rotateY(${ (instances.y.velocity/2) }deg)`;
+});
 
-function updateCursorPosition(e) {
-  mouse.x = e.pageX;
-  mouse.y = e.pageY;
-}
+// call kinet animate method on mousemove
+document.addEventListener('mousemove', function (event) {
+  kinet.animate('x', event.clientX - window.innerWidth/2);
+  kinet.animate('y', event.clientY - window.innerHeight/2);
+});
 
-function updateCursor() {
-  gsap.set([cursorInner, cursorTextContainerEl], {
-    x: mouse.x,
-    y: mouse.y
-  });
+// log
+kinet.on('start', function() {
+  console.log('start');
+});
 
-  gsap.to(cursorOuter, {
-    duration: 0.15,
-    x: mouse.x,
-    y: mouse.y
-  });
-
-  if (!isHovered) {
-    gsap.to(cursorTextContainerEl, hoverEffectDuration * 0.5, {
-      opacity: 0
-    });
-    gsap.set(cursorTextContainerEl, {
-      rotate: 0
-    });
-  }
-
-  requestAnimationFrame(updateCursor);
-}
-
-updateCursor();
-
-function handlePointerEnter(e) {
-  isHovered = true;
-
-  const target = e.currentTarget;
-  updateCursorText(target);
-
-  gsap.set([cursorTextContainerEl, cursorTextEl], {
-    height: initialCursorHeight,
-    width: initialCursorHeight
-  });
-
-  gsap.fromTo(
-    cursorTextContainerEl,
-    {
-      rotate: 0
-    },
-    {
-      duration: cursorRotationDuration,
-      rotate: 360,
-      ease: "none",
-      repeat: -1
-    }
-  );
-
-  gsap.to(cursorInner, hoverEffectDuration, {
-    scale: 2
-  });
-
-  gsap.fromTo(
-    cursorTextContainerEl,
-    hoverEffectDuration,
-    {
-      scale: 1.2,
-      opacity: 0
-    },
-    {
-      delay: hoverEffectDuration * 0.75,
-      scale: 1,
-      opacity: 1
-    }
-  );
-  gsap.to(cursorOuter, hoverEffectDuration, {
-    scale: 1.2,
-    opacity: 0
-  });
-}
-
-function handlePointerLeave() {
-  isHovered = false;
-  gsap.to([cursorInner, cursorOuter], hoverEffectDuration, {
-    scale: 1,
-    opacity: 1
-  });
-}
-
-function updateCursorText(textEl) {
-  const cursorTextRepeatTimes = textEl.getAttribute("data-cursor-text-repeat");
-  const cursorText = returnMultipleString(
-    textEl.getAttribute("data-cursor-text"),
-    cursorTextRepeatTimes
-  );
-
-  circleType.destroy();
-
-  cursorTextEl.innerHTML = cursorText;
-  circleType = new CircleType(cursorTextEl);
-}
-
-function returnMultipleString(string, count) {
-  let s = "";
-  for (let i = 0; i < count; i++) {
-    s += ` ${string} `;
-  }
-  return s;
-}
+kinet.on('end', function() {
+  console.log('end');
+});
