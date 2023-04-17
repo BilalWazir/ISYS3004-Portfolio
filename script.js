8e4b1cb9e17b02087902b49876c60a1ead0a3ff1
BilalWazir <101708971+BilalWazir@users.noreply.github.com>
2023-04-17 09:28:21 +0000

    changes to cursor

--- script.js
+++ script.js
@@ -30,12 +30,40 @@ document.addEventListener("mousemove", function(event) {
   document.getElementById("cursor").style.transform = "translate(" + x + "px, " + y + "px)";
 });
 
-var text = "Spinning Text";
-var textIndex = 0;
-setInterval(function() {
-  if (textIndex >= text.length) {
-    textIndex = 0;
-  }
-  document.getElementById("cursor").getElementsByClassName("text")[0].innerHTML = text.substring(textIndex, text.length) + text.substring(0, textIndex);
-  textIndex++;
-}, 100);
+/*Changes to Cursor*/
+
+// create instance of kinet with custom settings
+var kinet = new Kinet({
+  acceleration: 0.06,
+  friction: 0.20,
+  names: ["x", "y"],
+});
+
+// select circle element
+var circle = document.getElementById('circle');
+
+// set handler on kinet tick event
+kinet.on('tick', function(instances) {
+  circle.style.transform = `translate3d(${ (instances.x.current) }px, ${ (instances.y.current) }px, 0) rotateX(${ (instances.x.velocity/2) }deg) rotateY(${ (instances.y.velocity/2) }deg)`;
+});
+
+// call kinet animate method on mousemove
+document.addEventListener('mousemove', function (event) {
+  kinet.animate('x', event.clientX - window.innerWidth/2);
+  kinet.animate('y', event.clientY - window.innerHeight/2);
+});
+
+
+
+
+
+
+
+// log
+kinet.on('start', function() {
+  console.log('start');
+});
+
+kinet.on('end', function() {
+  console.log('end');
+});
